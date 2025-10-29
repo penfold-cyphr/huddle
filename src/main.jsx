a279a2ab509b0c3bfb4c8dfc18b1a1e9587189c5
penfold-cyphr <153822583+penfold-cyphr@users.noreply.github.com>
2025-10-29 15:53:54 +0000

    Ensure default import of App and import Tailwind styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <-- ADD THIS LINE TO IMPORT TAILWIND STYLES
+import App from './App.jsx' // Ensure this is a default import
+import './index.css' // Import Tailwind base styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
