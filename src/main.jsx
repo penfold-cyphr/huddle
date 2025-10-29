d75e055b74ae7ca8d112a12f57f73d62499b6d06
penfold-cyphr <153822583+penfold-cyphr@users.noreply.github.com>
2025-10-29 15:20:17 +0000

    Import Tailwind CSS in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Import the Tailwind CSS file
+import './index.css' // <-- ADD THIS LINE TO IMPORT TAILWIND STYLES
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
